7262b907d1dd7f9ed4e25e3a1053a882de4a494a
gmcglinn <loss4names@gmail.com>
2020-11-30 16:29:44 -0500

    removing client side aspect in branch for heroku launch

--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/index'
-import { VueCookieNext } from 'vue-cookie-next'
-
-
-createApp(App).use(store).use(router).use(VueCookieNext).mount('#app');
